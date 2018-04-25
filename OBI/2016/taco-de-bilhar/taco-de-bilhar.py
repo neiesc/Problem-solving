9ed2ef642c12dc399c89c35a57b3db615c5c8951
Edinei Cavalcanti <edinei.esc@gmail.com>
2018-04-25 14:48:44 -0300

    Add tacos de bilhar

---
+++ OBI/2016/taco-de-bilhar/taco-de-bilhar.py
@@ -0,0 +1,14 @@
+#https://olimpiada.ic.unicamp.br/pratique/p1/2016/f1/tacos-bilhar/
+
+tacos=[]
+fabricados=0
+c=int(input())
+comprimento_dos_tacos=list(map(int,input().rstrip().split()))
+for taco in comprimento_dos_tacos:
+    if taco in tacos:
+        tacos.remove(taco)
+    else:
+        tacos.append(taco)
+        fabricados+=2
+
+print(fabricados)
