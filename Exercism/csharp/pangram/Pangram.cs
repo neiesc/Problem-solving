e3498c5e59581a334f4ce66c2c7641bc7dd5c60f
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-03-22 17:57:57 -0300

    Add pangram

---
+++ Exercism/csharp/pangram/Pangram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        char[] alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        foreach (var alpha in alphas)
+        {
+            if (!input.Contains(alpha))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
