10a86f38b2e48208700a36fc42ae5df03d9618f0
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-03-24 18:38:04 -0300

    Add csharp isogram (#12)
    * Add csharp isogram side exercise
    
    * Implement IsIsogram and UnSkip tests

---
+++ Exercism/csharp/isogram/Isogram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        char[] alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        List<char> wordUsed = new List<char>();
+
+        foreach (var letter in word.ToUpper())
+        {
+            if (wordUsed.Contains(letter))
+            {
+                return false;
+            }
+
+            if (alphas.Contains(letter))
+            {
+                wordUsed.Add(letter);
+            }
+        }
+
+        return true;
+    }
+}
