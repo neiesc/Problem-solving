85a4446d1651390463014f2d8df7bb8954674047
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-03-24 11:23:59 -0300

    Unskip tests

--- Exercism/csharp/two-fer/TwoFerTest.cs
+++ Exercism/csharp/two-fer/TwoFerTest.cs
@@ -10,13 +10,13 @@ public class TwoFerTest
         Assert.Equal("One for you, one for me.", TwoFer.Name());
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void A_name_given()
     {
         Assert.Equal("One for Alice, one for me.", TwoFer.Name("Alice"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Another_name_given()
     {
         Assert.Equal("One for Bob, one for me.", TwoFer.Name("Bob"));
