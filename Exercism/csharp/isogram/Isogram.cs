44899ff2097acce2b81ccea3da1b5d6f58a1cdca
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-03-24 18:36:27 -0300

    Implement IsIsogram and UnSkip tests

--- Exercism/csharp/isogram/Isogram.cs
+++ Exercism/csharp/isogram/Isogram.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char[] alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        List<char> wordUsed = new List<char>();
+
+        foreach (var letter in word.ToUpper())
+        {
+            if (wordUsed.Contains(letter))
+            {
+                return false;
+            }
+
+            if (alphas.Contains(letter))
+            {
+                wordUsed.Add(letter);
+            }
+        }
+
+        return true;
     }
 }
