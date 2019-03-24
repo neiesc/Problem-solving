032f702e852f4b18ca8f53e414b0e0c1a754cdc0
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-03-24 14:12:03 -0300

    Solve leap

--- Exercism/csharp/leap/Leap.cs
+++ Exercism/csharp/leap/Leap.cs
@@ -4,6 +4,6 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return year % 4 == 0 && (year % 100 > 0 || year % 400 == 0);
     }
 }
--- Exercism/csharp/leap/LeapTest.cs
+++ Exercism/csharp/leap/LeapTest.cs
@@ -10,31 +10,31 @@ public class LeapTest
         Assert.False(Leap.IsLeapYear(2015));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_2_not_divisible_by_4_in_common_year()
     {
         Assert.False(Leap.IsLeapYear(1970));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_4_not_divisible_by_100_in_leap_year()
     {
         Assert.True(Leap.IsLeapYear(1996));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_100_not_divisible_by_400_in_common_year()
     {
         Assert.False(Leap.IsLeapYear(2100));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_400_in_leap_year()
     {
         Assert.True(Leap.IsLeapYear(2000));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_200_not_divisible_by_400_in_common_year()
     {
         Assert.False(Leap.IsLeapYear(1800));
