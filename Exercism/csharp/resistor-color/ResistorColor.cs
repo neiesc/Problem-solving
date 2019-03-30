2a28756fca73e61a93e17d52060282d33049074e
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-03-30 16:58:57 -0300

    Add csharp resistor color (#16)
    * Add csharp resistor color
    
    * Implement exercism and UnSkip tests

---
+++ Exercism/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        return Array.FindIndex(Colors(), x => x == color);
+    }
+
+    public static string[] Colors()
+    {
+        return new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    }
+}
