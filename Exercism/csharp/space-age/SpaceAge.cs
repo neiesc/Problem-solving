b91f69eee40546caa44abba1dd54a4844d299888
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-03-30 21:42:49 -0300

    Solve solution and Unskip tests and add digit separators

--- Exercism/csharp/space-age/SpaceAge.cs
+++ Exercism/csharp/space-age/SpaceAge.cs
@@ -2,47 +2,51 @@ using System;
 
 public class SpaceAge
 {
+    public const double EarthOrbitalPeriodSecondsPerYear = 365.25 * 24 * 60 * 60;
+    public int SecondsTotal { get; set; }
+
     public SpaceAge(int seconds)
     {
+        SecondsTotal = seconds;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / EarthOrbitalPeriodSecondsPerYear;
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / (0.2408467 * EarthOrbitalPeriodSecondsPerYear);
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / (0.61519726 * EarthOrbitalPeriodSecondsPerYear);
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / (1.8808158 * EarthOrbitalPeriodSecondsPerYear);
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / (11.862615 * EarthOrbitalPeriodSecondsPerYear);
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / (29.447498 * EarthOrbitalPeriodSecondsPerYear);
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / (84.016846 * EarthOrbitalPeriodSecondsPerYear);
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SecondsTotal / (164.79132 * EarthOrbitalPeriodSecondsPerYear);
     }
 }
--- Exercism/csharp/space-age/SpaceAgeTest.cs
+++ Exercism/csharp/space-age/SpaceAgeTest.cs
@@ -7,56 +7,56 @@ public class SpaceAgeTest
     [Fact]
     public void Age_on_earth()
     {
-        var sut = new SpaceAge(1000000000);
+        var sut = new SpaceAge(1_000_000_000);
         Assert.Equal(31.69, sut.OnEarth(), precision: 2);
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_mercury()
     {
-        var sut = new SpaceAge(2134835688);
+        var sut = new SpaceAge(2_134_835_688);
         Assert.Equal(280.88, sut.OnMercury(), precision: 2);
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_venus()
     {
-        var sut = new SpaceAge(189839836);
+        var sut = new SpaceAge(189_839_836);
         Assert.Equal(9.78, sut.OnVenus(), precision: 2);
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_mars()
     {
-        var sut = new SpaceAge(2129871239);
+        var sut = new SpaceAge(2_129_871_239);
         Assert.Equal(35.88, sut.OnMars(), precision: 2);
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_jupiter()
     {
-        var sut = new SpaceAge(901876382);
+        var sut = new SpaceAge(901_876_382);
         Assert.Equal(2.41, sut.OnJupiter(), precision: 2);
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_saturn()
     {
-        var sut = new SpaceAge(2000000000);
+        var sut = new SpaceAge(2_000_000_000);
         Assert.Equal(2.15, sut.OnSaturn(), precision: 2);
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_uranus()
     {
-        var sut = new SpaceAge(1210123456);
+        var sut = new SpaceAge(1_210_123_456);
         Assert.Equal(0.46, sut.OnUranus(), precision: 2);
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Age_on_neptune()
     {
-        var sut = new SpaceAge(1821023456);
+        var sut = new SpaceAge(1_821_023_456);
         Assert.Equal(0.35, sut.OnNeptune(), precision: 2);
     }
 }
