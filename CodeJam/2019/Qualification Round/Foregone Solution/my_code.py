fd6b8e85afd518b6628f9857f8db50678bece984
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-04-07 16:30:31 -0300

    Change Foregone Solution the O(n²) to O(n)

--- CodeJam/2019/Qualification Round/Foregone Solution/my_code.py
+++ CodeJam/2019/Qualification Round/Foregone Solution/my_code.py
@@ -1,11 +1,17 @@
-def find_result_diferent_four(x):
-    for a in range(x, 0, -1):
-        for b in range(x, 0, -1):
-            if ((a + b) == x) and ("4" not in str(a) and "4" not in str(b)):
-                return (a, b)
+def solve(n):
+    a, b = '', ''
+    n = list(n)
+    for i in n:
+        if i == '4':
+            a = a + '3'
+            b = b + '1'
+        else:
+            a = a + i
+            b = b + '0'
+    return int(a), int(b)
 
 t = int(input())
 for x in range(t):
-    n = int(input())
-    a, b = find_result_diferent_four(n)
+    n = input()
+    a, b = solve(n)
     print("Case #{:d}: {:d} {:d}".format(x + 1, a, b))
