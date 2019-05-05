a7a312ff61e4ad75bade547ead83c07626ef0a9d
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-05-05 17:00:44 -0300

    Implement first robot name

--- Exercism/csharp/robot-name/RobotName.cs
+++ Exercism/csharp/robot-name/RobotName.cs
@@ -1,17 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot
 {
+    private string _name { get; set; }
+    private static HashSet<string> _names = new HashSet<string>();
+    public Robot()
+    {
+        _name = GenerateNewName();
+        while (!_names.Add(_name))
+        {
+            _name = GenerateNewName();
+        }
+    }
+
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return _name;
         }
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _name = GenerateNewName();
+        while (!_names.Add(_name))
+        {
+            _name = GenerateNewName();
+        }
     }
+
+    #region Helpers
+
+    private string GenerateNewName()
+    {
+        var random = new Random();
+
+        var newName = GenerateCharAlphabetUpperCaseRandom() + GenerateCharAlphabetUpperCaseRandom() + random.Next(100, 999).ToString();
+        return newName;
+    }
+
+    private string GenerateCharAlphabetUpperCaseRandom()
+    {
+        var random = new Random();
+        return Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65))).ToString();
+    }
+
+    #endregion Helpers
 }
