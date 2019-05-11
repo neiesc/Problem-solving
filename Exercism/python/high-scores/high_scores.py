763f63eb16b11df13fe00f9dc84c46e59327450b
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-05-11 09:44:50 -0300

    Implement rule of high scores

--- Exercism/python/high-scores/high_scores.py
+++ Exercism/python/high-scores/high_scores.py
@@ -1,10 +1,12 @@
 def latest(scores):
-    pass
+    return min([score for score in scores if score > 0])
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    scores_sorted = sorted(scores)
+    scores_sorted.reverse()
+    return scores_sorted[:3]
