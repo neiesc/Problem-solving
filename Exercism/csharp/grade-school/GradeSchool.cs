ec92a386c469f529203a062ba52e9e1f51fdf2e7
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-06-02 13:13:34 -0300

    Unskip tests and implement solution

--- Exercism/csharp/grade-school/GradeSchool.cs
+++ Exercism/csharp/grade-school/GradeSchool.cs
@@ -1,20 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+    public Student(string name, int grade)
+    {
+        Name = name;
+        Grade = grade;
+    }
+
+    public string Name { get; set; }
+    public int Grade { get; set; }
+}
 
 public class GradeSchool
 {
+
+
+    private List<Student> students = new List<Student>();
+
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        students.Add(new Student(student, grade));
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return students.OrderBy(x => x.Grade).ThenBy(x => x.Name).Select(x => x.Name);
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return students.Where(x => x.Grade == grade).OrderBy(x => x.Name).Select(x => x.Name);
     }
 }
