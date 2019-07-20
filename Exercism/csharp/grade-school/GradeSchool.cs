4e02ed763e13de5ce660f628ef80fa12a1f8e280
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-07-20 13:53:10 -0300

    Add exercism csharp grade school (#38)
    * Add Grade School
    
    * Unskip tests and implement solution
    
    * space
    
    * Add support of immutability

---
+++ Exercism/csharp/grade-school/GradeSchool.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+    public Student(string name, int grade)
+    {
+        Name = name;
+        Grade = grade;
+    }
+
+    public string Name { get; set; }
+    public int Grade { get; set; }
+}
+
+public class GradeSchool
+{
+
+
+    private readonly List<Student> students = new List<Student>();
+
+
+    public void Add(string student, int grade)
+    {
+        students.Add(new Student(student, grade));
+    }
+
+    public IEnumerable<string> Roster()
+    {
+        return students
+        .OrderBy(x => x.Grade)
+        .ThenBy(x => x.Name)
+        .Select(x => x.Name);
+    }
+
+    public IEnumerable<string> Grade(int grade)
+    {
+        return students
+            .Where(x => x.Grade == grade)
+            .OrderBy(x => x.Name)
+            .Select(x => x.Name);
+    }
+}
