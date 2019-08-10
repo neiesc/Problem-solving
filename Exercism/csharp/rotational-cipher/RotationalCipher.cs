d141e18c3ce5b3ece5420a4185c072c96ac48f76
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-08-10 12:08:01 -0300

    Using dict in RotationalCipher (analize)

--- Exercism/csharp/rotational-cipher/RotationalCipher.cs
+++ Exercism/csharp/rotational-cipher/RotationalCipher.cs
@@ -10,33 +10,36 @@ public static class RotationalCipher
     
     public static string Rotate(string text, int shiftKey)
     {
+        var cipher = new Dictionary<char, char>();
+        
         var cipher_lower = new List<char>();
         cipher_lower.AddRange(_alphas_lower.ToList().GetRange(shiftKey, _alphas_lower.Count() - shiftKey));
         cipher_lower.AddRange(_alphas_lower.ToList().GetRange(0, shiftKey));
+
+        for (int i = 0; i < _alphas_lower.Length; i++)
+        {
+            cipher.Add(_alphas_lower[i], cipher_lower[i]);
+        }
 
         var cipher_upper = new List<char>();
         cipher_upper.AddRange(_alphas_upper.ToList().GetRange(shiftKey, _alphas_upper.Count() - shiftKey));
         cipher_upper.AddRange(_alphas_upper.ToList().GetRange(0, shiftKey));
 
+        for (int i = 0; i < _alphas_upper.Length; i++)
+        {
+            cipher.Add(_alphas_upper[i], cipher_upper[i]);
+        }
+
         StringBuilder text_cipher = new StringBuilder();
         foreach (var c in text)
         {
-            var index = _alphas_lower.ToList().IndexOf(c);
-            if (index >= 0)
+            if (cipher.TryGetValue(c, out var value))
             {
-                text_cipher.Append(cipher_lower[index]);                
+                text_cipher.Append(value);
             }
             else
             {
-                index = _alphas_upper.ToList().IndexOf(c);
-                if (index >= 0)
-                {
-                    text_cipher.Append(cipher_upper[index]);
-                }
-                else
-                {
-                    text_cipher.Append(c);
-                }
+                text_cipher.Append(c);
             }
         }
 
