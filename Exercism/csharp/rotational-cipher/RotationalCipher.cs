5fabe9f79ddc58ce980b018c61708ff7e6da9ee4
Edinei Cavalcanti <edinei.esc@gmail.com>
2019-08-10 11:42:49 -0300

    Use StringBuilder in Rotational Cipher

--- Exercism/csharp/rotational-cipher/RotationalCipher.cs
+++ Exercism/csharp/rotational-cipher/RotationalCipher.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text;
 
 public static class RotationalCipher
 {
@@ -17,28 +18,28 @@ public static class RotationalCipher
         cipher_upper.AddRange(_alphas_upper.ToList().GetRange(shiftKey, _alphas_upper.Count() - shiftKey));
         cipher_upper.AddRange(_alphas_upper.ToList().GetRange(0, shiftKey));
 
-        var text_cipher = "";
+        StringBuilder text_cipher = new StringBuilder();
         foreach (var c in text)
         {
             var index = _alphas_lower.ToList().IndexOf(c);
             if (index >= 0)
             {
-                text_cipher += cipher_lower[index];                
+                text_cipher.Append(cipher_lower[index]);                
             }
             else
             {
                 index = _alphas_upper.ToList().IndexOf(c);
                 if (index >= 0)
                 {
-                    text_cipher += cipher_upper[index];
+                    text_cipher.Append(cipher_upper[index]);
                 }
                 else
                 {
-                    text_cipher += c;
+                    text_cipher.Append(c);
                 }
             }
         }
 
-        return text_cipher;
+        return text_cipher.ToString();
     }
 }
