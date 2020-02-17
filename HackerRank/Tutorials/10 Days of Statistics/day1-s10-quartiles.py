ce830e70c36f95e803b713ca433387f61caff271
Edinei <edinei3A@hotmail.com>
2020-02-16 22:03:40 -0300

    Test

--- HackerRank/Tutorials/10 Days of Statistics/day1-s10-quartiles.py
+++ HackerRank/Tutorials/10 Days of Statistics/day1-s10-quartiles.py
@@ -4,6 +4,7 @@
 
 def median_q2(number_of_elements, elements):
     elements_sorted = sorted(elements)
+    print(f"elements_sorted: {elements_sorted}")
     middle_elements = int(number_of_elements/2)
     is_even = number_of_elements % 2 == 0
 
@@ -19,15 +20,25 @@ def median(elements_sorted):
     return (elements_sorted[middle_elements - 1] + elements_sorted[middle_elements])/2
 
 def solve(number_of_elements, elements):
+    is_even = number_of_elements % 2 == 0
+
+    index_offset_q1 = 0
+    index_offset_q3 = 1
+    if is_even:
+        index_offset_q1 = 1
+        index_offset_q3 = 1
+
     median_result = median_q2(number_of_elements, elements)
     q2 = median_result[0]
-    q1 = median(median_result[2][:median_result[1]])
-    q3 = median(median_result[2][median_result[1] + 1:])
+    q1 = median(median_result[2][:median_result[1] - index_offset_q1])
+    q3 = median(median_result[2][median_result[1] + index_offset_q3:])
+    print(f"q1: {median_result[2][:median_result[1] - index_offset_q1]}")
+    print(f"q3: {median_result[2][median_result[1] + index_offset_q3:]}")
 
     return (int(q1) , int(q2), int(q3))
 
 if __name__ == "__main__":
-    number_of_elements = int(input())
-    elements = list(map(int, input().rstrip().split()))
+    number_of_elements = 10 #int(input())
+    elements = list(map(int, "3 7 8 5 12 14 21 15 18 14".rstrip().split()))
 
     print("\n".join(map(str,solve(number_of_elements, elements))))
