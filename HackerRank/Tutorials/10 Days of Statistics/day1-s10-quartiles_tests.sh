5484b6fd763c32f9f6df58f101c3c492e572f3b8
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-04-08 22:42:03 -0300

    Refactory and add tests

---
+++ HackerRank/Tutorials/10 Days of Statistics/day1-s10-quartiles_tests.sh
@@ -0,0 +1,3 @@
+python day1-s10-quartiles.py < day1-s10-quartiles_test_0_input.txt > day1-s10-quartiles_test_0_output.txt
+python day1-s10-quartiles.py < day1-s10-quartiles_test_1_input.txt > day1-s10-quartiles_test_1_output.txt
+python day1-s10-quartiles.py < day1-s10-quartiles_test_2_input.txt > day1-s10-quartiles_test_2_output.txt
