31c321d13d053618659db52aebb9509839e35489
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-04-22 20:52:53 -0300

    HackerRank/Find a string

---
+++ HackerRank/Python/Strings/find-a-string.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+# Find a string
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(0, len(string)):
+        word = string[i: i + len(sub_string)]
+        if word == sub_string:
+            count += 1
+
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
