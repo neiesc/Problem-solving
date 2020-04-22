bb87d3aac7892596b9d3eb639abb558e4f505de7
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-04-22 20:01:51 -0300

    HackerRank/sWAP cASE

---
+++ HackerRank/Python/Strings/swap-case.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+# sWAP cASE
+# hackerrank.com/challenges/swap-case/problem
+
+def swap_case(s):
+    ret = ''
+
+    for letter in s:
+        if letter.islower():
+            ret += letter.upper()
+        else:
+            ret += letter.lower()
+    return ret
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
