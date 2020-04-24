8cd91ec12798eb373e5fd5ba49bb92cdd6d207be
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-04-24 20:50:20 -0300

    hackerrank/python/write-a-function

---
+++ HackerRank/Python/Introduction/write-a-function.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+# Write a function
+# https://www.hackerrank.com/challenges/write-a-function/problem
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0:
+        leap = True
+    if year % 100 == 0:
+        leap = False
+    if year % 400 == 0:
+        leap = True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
