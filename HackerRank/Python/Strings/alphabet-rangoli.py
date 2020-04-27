dfda0dd8c257813ea7c3e13175706b944e7112f5
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-04-27 19:26:43 -0300

    Fix algoritm

--- HackerRank/Python/Strings/alphabet-rangoli.py
+++ HackerRank/Python/Strings/alphabet-rangoli.py
@@ -9,11 +9,20 @@ def print_rangoli(size):
     lascii_lowercase = lascii_lowercase[:size]
     lascii_lowercase_reverse = list(lascii_lowercase)
     lascii_lowercase_reverse.reverse()
-    print(lascii_lowercase)
-    print(lascii_lowercase_reverse)
-    for i in range(size - 1):
-        l = lascii_lowercase_reverse[:i + 1] + lascii_lowercase[size - 1 : i + 2]
-        print("-".join(l).center(size * 3, "-"))
+
+    for i in range(size -1):
+        lr = list(lascii_lowercase_reverse[:i + 1])
+        lr.pop()
+        lr.reverse()
+        l = lascii_lowercase_reverse[:i + 1] + lr
+        print("-".join(l).center((size * 4) - 3, "-"))
+
+    for i in range(size - 1, -1, -1):
+        lr = list(lascii_lowercase_reverse[:i + 1])
+        lr.pop()
+        lr.reverse()
+        l = lascii_lowercase_reverse[:i + 1] + lr
+        print("-".join(l).center((size * 4) - 3, "-"))
 
 if __name__ == '__main__':
     n = int(input())
