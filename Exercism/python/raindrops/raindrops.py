0f295d0ee8c29361bd4f80dbc947da65dd7fbbe6
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-05-09 11:05:09 -0300

    move raindrops

--- Exercism/python/raindrops/raindrops.py
+++ Exercism/python/raindrops/raindrops.py
@@ -1,5 +1,6 @@
-raindrops = ((3, "Pling"), (5, "Plang"), (7, "Plong"))
 def convert(number):
+    raindrops = ((3, "Pling"), (5, "Plang"), (7, "Plong"))
+    
     raindrop_result = [raindrop[1] for raindrop in raindrops if number % raindrop[0] == 0]
 
     return "".join(raindrop_result) or str(number)
