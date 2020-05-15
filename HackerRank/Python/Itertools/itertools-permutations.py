6672139e0d10ac391c7c212b7f743447fceec8e3
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-05-15 20:25:33 -0300

    hackerrank/python/itertools-permutations

---
+++ HackerRank/Python/Itertools/itertools-permutations.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+# itertools.permutations()
+# https://www.hackerrank.com/challenges/itertools-permutations/problem
+
+from itertools import permutations
+
+if __name__ == '__main__':
+    a, r = input().split()
+    r = int(r)
+
+    l = list(a)
+
+    print("\n".join(map(lambda x: "".join(x) ,permutations(sorted(l), r))))
