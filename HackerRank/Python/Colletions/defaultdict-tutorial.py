718a9017d946985f013cba56ac211173cb6af191
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-05-26 22:02:23 -0300

    Fix: defaultdict-tutorial

--- HackerRank/Python/Colletions/defaultdict-tutorial.py
+++ HackerRank/Python/Colletions/defaultdict-tutorial.py
@@ -4,24 +4,19 @@
 
 from collections import defaultdict
 
-def solve(d):
-    d_result = defaultdict(list)
-    for word_group in d['B']:
-        for index, word_appeared in enumerate(d['A'], start=1):
-            if word_appeared == word_group:
-                d_result[word_group].append(str(index))
-
-        if not word_group in d_result:
-            d_result[word_group].append("-1")
-    
-    return d_result
-
 if __name__ == '__main__':
-    n, m = input().split()
     d = defaultdict(list)
-    for _ in range(int(n)):
-        d['A'].append(input())
+    l = []
 
-    for _ in range(int(m)):
-        d['B'].append(input())
-    print("\n".join(map(lambda x: " ".join(x), solve(d).values())))
+    n,m=[int(x) for x in input().split(' ')]
+    for index in range(1, n + 1):
+        d[input()].append(index)
+
+    for _ in range(m):
+        l.append(input())
+
+    for item in l :
+        if item in d :
+            print(" ".join(map(str, d[item])))
+        else:
+            print(-1)
