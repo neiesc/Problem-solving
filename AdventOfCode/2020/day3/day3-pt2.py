643021c8bf9dec9537f838bb56f27faeac17824e
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-12-20 10:24:56 -0300

    Finish

--- AdventOfCode/2020/day3/day3-pt2.py
+++ AdventOfCode/2020/day3/day3-pt2.py
@@ -1,39 +1,33 @@
 def is_tree(width, line, right):
-    print(f"line: {line}")
-    print(f"right % width: {right % width}")
-    if line[right % width] == "#":
-        return True
-
-    #print(line[line_number * 3])
-    #print(line)
-    #print(line_number * 3)
-
-    return False
+    line_debug = list(line)
+    line_debug[right % width] = "X" if line[right % width] == "#" else "O"
+    #print(f"line: {''.join(line_debug)}")
+    return line[right % width] == "#"
 
 def total_tree(right, down):
     total_tree = 0
 
-    with open("input_test.txt", "r") as f:
-        right_current = right
-        down_current = down
+    with open("input.txt", "r") as f:
+        right_current = 0
+        down_current = 0
         lines = f.readlines()
-        width = len(lines[0])
+        width = len(lines[0].rstrip())
         height = len(lines)
         while down_current < height:
-            line = lines[down_current].replace("\n", "")
-            if is_tree(width, line, right_current + 1):
+            line = lines[down_current].rstrip()
+            if is_tree(width, line, right_current):
                 total_tree += 1
             right_current  += right
             down_current += down
-            print(f"right_current: {right_current}")
-            print(f"down_current: {down_current}")
+            #print(f"right_current: {right_current}")
+            #print(f"down_current: {down_current}")
 
     return total_tree
 
-# result = total_tree(1, 1)
-result = total_tree(3, 1)
-# result *= total_tree(5, 1)
-# result *= total_tree(7, 1)
-# result *= total_tree(1, 2)
+result = total_tree(1, 1)
+result *= total_tree(3, 1)
+result *= total_tree(5, 1)
+result *= total_tree(7, 1)
+result *= total_tree(1, 2)
 
 print(f"result: {result}")
