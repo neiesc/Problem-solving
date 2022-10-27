0a51b220ac71d24b325972cabdb279903213901b
Edinei Cavalcanti <hi@edinei.dev>
2022-10-26 21:53:06 -0300

    add clock with last version of exercism/csharp/clock

---
+++ Exercism/csharp/clock/Clock.cs
@@ -0,0 +1,84 @@
+using System;
+
+public class Clock
+{
+    private int _hours;
+    private int _minutes;
+
+    public Clock(int hours, int minutes)
+    {
+        var hoursAndminutes = CalculeMinutesAndHours(hours, minutes);
+        
+        _hours = hoursAndminutes.Item1;
+        _minutes = hoursAndminutes.Item2;
+    }
+
+    public Clock Add(int minutesToAdd)
+    {
+        var hoursAndminutes = CalculeMinutesAndHours(_hours, _minutes + minutesToAdd);
+
+        return new Clock(hoursAndminutes.Item1, hoursAndminutes.Item2);
+    }
+
+    public Clock Subtract(int minutesToSubtract)
+    {
+        var hoursAndminutes = CalculeMinutesAndHours(_hours, _minutes - minutesToSubtract);
+
+        return new Clock(hoursAndminutes.Item1, hoursAndminutes.Item2);
+    }
+
+    public override string ToString()
+    {
+        return $"{_hours.ToString("d2")}:{_minutes.ToString("d2")}";
+    }
+
+    public override bool Equals(Object obj)
+    {
+        if ((obj == null) || ! this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        } else {
+            return ToString() == obj.ToString();
+        }
+   }
+
+    private static (int, int) CalculeMinutesAndHours(int hours, int minutes)
+    {
+        if (minutes >= 60)
+        {
+            hours = hours + (minutes / 60);
+            minutes = minutes % 60;
+        }
+        else if (minutes < 0)
+        {
+            var minutesToHours = Math.Abs(minutes / 60);
+            hours -= minutesToHours;
+            if (minutes < 0)
+            {
+                minutes += minutesToHours * 60;
+            }
+            else
+            {
+                minutes -= minutesToHours * 60;
+            }
+        }
+
+        hours = hours % 24;
+        if (hours < 0)
+        {
+            hours = 24 - Math.Abs(hours);
+        }
+
+        if (minutes < 0)
+        {
+            hours -= 1;
+            if (hours < 0)
+            {
+                hours = 24 - Math.Abs(hours);
+            }
+            minutes = 60 - Math.Abs(minutes);
+        }
+
+        return (hours, minutes);
+    }
+}
