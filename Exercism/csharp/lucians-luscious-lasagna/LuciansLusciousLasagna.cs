d295b76159298d7fa8f5c963d0823dee4bf285e7
Edinei Cavalcanti <hi@edinei.dev>
2025-09-19 13:45:48 -0300

    refactory exercism Lucian's Luscious Lasagna #2

--- Exercism/csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ Exercism/csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -2,23 +2,11 @@ class Lasagna
 {
     private const int MinutesInOven = 40;
 
-    public int ExpectedMinutesInOven()
-    {
-        return MinutesInOven;
-    }
+    public int ExpectedMinutesInOven() => MinutesInOven;
 
-    public int RemainingMinutesInOven(int actualMinutes)
-    {
-        return MinutesInOven - actualMinutes;
-    }
+    public int RemainingMinutesInOven(int actualMinutes) => MinutesInOven - actualMinutes;
 
-    public int PreparationTimeInMinutes(int preparationSteps)
-    {
-        return preparationSteps * 2;
-    }
+    public int PreparationTimeInMinutes(int preparationSteps) => preparationSteps * 2;
 
-    public int ElapsedTimeInMinutes(int preparationSteps, int actualMinutes)
-    {
-        return PreparationTimeInMinutes(preparationSteps) + actualMinutes;
-    }
+    public int ElapsedTimeInMinutes(int preparationSteps, int actualMinutes) => PreparationTimeInMinutes(preparationSteps) + actualMinutes;
 }
